bb7d44e102038ecd5d486c4cee24301cfe66f6ba
ANIMEAARIZWORLD <mohdaariz248@gmail.com>
2025-09-11 12:53:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
+const CACHE_NAME = "bae-shop-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+// Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("bae-shop-cache").then((cache) => {
-      return cache.addAll([
-        "./GHODA.HTML",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch Requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
